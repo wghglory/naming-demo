a3a9eccd870eb6b29920ba64bc5e900d26a224e6
Guanghui Wang <guanghui-wang@foxmail.com>
2022-09-27 15:58:28 +0800

    feat: use baby-naming v2

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module namingdemo
 go 1.19
 
 require github.com/wghglory/baby-naming v1.0.4
+
+require github.com/wghglory/baby-naming/v2 v2.0.1
--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 
 	naming "github.com/wghglory/baby-naming/naming"
+	naming2 "github.com/wghglory/baby-naming/v2/naming"
 )
 
 func main() {
-	name := naming.CreateBabyName()
-	fmt.Println("A random baby name:", name)
+	fmt.Println("v1 --- A random baby name:", naming.CreateBabyName())
+
+	fmt.Println("v2 --- A random female baby name:", naming2.CreateBabyName(false))
+	fmt.Println("v2 --- A random male baby name:", naming2.CreateBabyName(true))
 }

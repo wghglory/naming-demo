c169a3325b61da2e5d75d705c22f2475f2261bea
Guanghui Wang <guanghui-wang@foxmail.com>
2022-09-27 16:34:32 +0800

    feat: use v3

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module namingdemo
 
 go 1.19
 
-require github.com/wghglory/baby-naming v1.0.4
 
-require github.com/wghglory/baby-naming/v2 v2.0.1
+require (
+  github.com/wghglory/baby-naming v1.0.4
+	github.com/wghglory/baby-naming/v2 v2.0.1
+	github.com/wghglory/baby-naming/v3 v3.0.0
+)
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	naming "github.com/wghglory/baby-naming/naming"
 	naming2 "github.com/wghglory/baby-naming/v2/naming"
+	naming3 "github.com/wghglory/baby-naming/v3/naming"
 )
 
 func main() {
@@ -12,4 +13,26 @@ func main() {
 
 	fmt.Println("v2 --- A random female baby name:", naming2.CreateBabyName(false))
 	fmt.Println("v2 --- A random male baby name:", naming2.CreateBabyName(true))
+
+	// ----------------------------below v3------------------------------------
+	male, maleErr := naming3.CreateBabyName(true, 4)
+	if maleErr == nil {
+		fmt.Printf("v3 --- male baby found (min length %v): %v\n", 4, male)
+	} else {
+		fmt.Println("v3 --- ", maleErr)
+	}
+
+	female, femaleErr := naming3.CreateBabyName(false, 3)
+	if femaleErr == nil {
+		fmt.Printf("v3 --- female baby found (min length %v): %v\n", 3, female)
+	} else {
+		fmt.Println("v3 --- ", femaleErr)
+	}
+
+	baby, err := naming3.CreateBabyName(false, 30)
+	if err == nil {
+		fmt.Printf("v3 --- female baby found (min length %v): %v\n", 30, baby)
+	} else {
+		fmt.Println("v3 --- ", err)
+	}
 }
